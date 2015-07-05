e43fe78ef28e09b599297d7f289b4d1181f8a9b5
Kei Yoshimura <keiichiro.mac@gmail.com>
2015-07-05 21:38:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+seismic-intensity-map
+======
+
+Seismic Intensity Map Rendering using Earthquake Information from JMA(Japan Meteorological Agency) by OpenLayers 3
+
+気象庁防災情報XML電文の震度速報、震源・震度に関する情報から生成したGeoJSONを用いて、区域別の震度(数値)を当該区域上にマッピングします。
+
+![sample](http://www.quitsq.com/images/seismic-intensity-map-sample.gif)
+
+* 縮尺に応じて細分区域(188区域)と市町村等(1898区域)の表示が切り替わります。
+* [jma-eqxml2geojson](https://github.com/9SQ/jma-eqxml2geojson)で生成した2つのGeoJSONが必要です。
+* 描画にはOpenLayers 3を使用しています。
+
+### ファイル
+
+* **maprenderer.js** : 本体
+* images/*.png : 各震度と震源地のアイコン
+
+
+* viewer.html : サンプルhtml
+* largeScalePoints.json : サンプルGeoJSON
+* smallScalePoints.json : サンプルGeoJSON
+
+GeoJSON (2015年5月30日 20:24分頃に小笠原諸島西方沖で発生した最大震度5強の地震のXML電文から生成したもの) をサンプルとして含めているので、viewer.htmlで表示を確認できます。
---
+++ maprenderer.js
@@ -0,0 +1,121 @@
+//震源地と各震度のアイコンの定義
+var styles = {
+    'epicenter': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/epicenter.png'
+        }))
+    })],
+    '1': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int1.png'
+        }))
+    })],
+    '2': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int2.png'
+        }))
+    })],
+    '3': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int3.png'
+        }))
+    })],
+    '4': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int4.png'
+        }))
+    })],
+    '5': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int5l.png'
+        }))
+    })],
+    '6': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int5h.png'
+        }))
+    })],
+    '7': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int6l.png'
+        }))
+    })],
+    '8': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int6h.png'
+        }))
+    })],
+    '9': [new ol.style.Style({
+        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */ ({
+            scale: 0.5,
+            src: 'images/int7.png'
+        }))
+    })],
+};
+
+//GeoJSONのproperties->titleに応じてstyleを反映
+var styleFunction = function(feature, resolution) {
+    return styles[feature.get('class')];
+};
+
+//広域ポイント(細分区域)レイヤーの定義
+var smallScalePoints = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'smallScalePoints.json',
+        format: new ol.format.GeoJSON()
+    }),
+    style: styleFunction,
+    //表示する解像度(縮尺)
+    minResolution: 1000,
+    maxResolution: 20000
+});
+
+//詳細ポイント(市町村等)レイヤーの定義
+var largeScalePoints = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'largeScalePoints.json',
+        format: new ol.format.GeoJSON()
+    }),
+    style: styleFunction,
+    //表示する解像度(縮尺)
+    minResolution: 20,
+    maxResolution: 1000
+});
+
+var map = new ol.Map({
+    target: 'map',
+    renderer: 'canvas',
+    layers: [
+        //マップのレイヤー
+        new ol.layer.Tile({
+            source: new ol.source.OSM
+        }),
+        //ポイント(細分区域)
+        smallScalePoints,
+        //ポイント(市町村等)
+        largeScalePoints
+    ],
+    view: new ol.View({
+        //中心座標(仮指定)
+        center: ol.proj.transform([134.15, 35.27], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 6
+    })
+});
+
+//広域ポイント(細分区分)から最適なズームレベルと中心座標を設定
+var source = smallScalePoints.getSource();
+var onChangeKey = source.on('change', function() {
+  if (source.getState() == 'ready') {
+    source.unByKey(onChangeKey);
+    map.getView().fitExtent(source.getExtent(), map.getSize());
+  }
+});
